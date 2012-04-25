e3c5dbe256a279b06064ba1ad65427d7735f1182
Raphael Jasjukaitis <r.jasjukaitis@raphaa.de>
2012-04-25 14:20:32 +0200

    Pagination for plugin added.

--- cmsplugin_photologue_pro/cms_plugins.py
+++ cmsplugin_photologue_pro/cms_plugins.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.sites.models import Site
+from django.core.paginator import EmptyPage, InvalidPage, Paginator
 from django.utils.translation import ugettext_lazy as _
 
 from cms.models.pagemodel import Page
@@ -18,10 +19,22 @@ class AlbumPlugin(CMSPluginBase):
     render_template = 'cmsplugin_photologue_pro/album_plugin.html'
 
     def render(self, context, instance, placeholder):
-        photos = instance.album.photos.all().order_by('id')
+        photo_instance = instance.album.photos.all().order_by('id')
+        per_page = instance.per_page or 100
+        paginator = Paginator(photo_instance, per_page)
+        try:
+            page = max(int(context.get('request', 1).GET.get('page', 1)))
+        except (ValueError, TypeError):
+            page = 1
+        try:
+            photos = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            photos = paginator.page(paginator.num_pages)
         context.update({
             'gallery': instance.album,
             'photos': photos,
+            'pages': paginator.page_range,
+            'current_page': page,
         })
         return context
 
--- cmsplugin_photologue_pro/models.py
+++ cmsplugin_photologue_pro/models.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
+
+PHOTOS_PER_PAGE = (
+    (10, 10),
+    (25, 25),
+    (50, 50),
+    (100, 100),
+    (250, 250),
+)
 
 class Album(CMSPlugin):
     """Model for Album CMS plugin."""
 
-    album = models.ForeignKey('photologue.Gallery')
+    album = models.ForeignKey('photologue.Gallery', verbose_name=_('Album'))
+    per_page = models.IntegerField(_('Photos per page'),
+                                   choices=PHOTOS_PER_PAGE, default=100,
+                                   max_length=3)
