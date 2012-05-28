7c75ffd498009f11b6ffbae53d43d5e963a06d7d
Raphael Jasjukaitis <r.jasjukaitis@raphaa.de>
2012-05-28 13:00:53 +0200

    URL names updated.

--- cmsplugin_photologue_pro/urls.py
+++ cmsplugin_photologue_pro/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('cmsplugin_photologue_pro.views',
-    url(r'^$', 'overview', name='overview'),
-    url(r'^(?P<album>\d)/$', 'album', name='album'),
-    url(r'^(?P<album>\d)/(?P<photo>\d)/$', 'photo', name='photo'),
+    url(r'^$', 'overview', name='photologue_overview'),
+    url(r'^(?P<album>\d)/$', 'album', name='photologue_album'),
+    url(r'^(?P<album>\d)/(?P<photo>\d)/$', 'photo', name='photologue_photo'),
 )
