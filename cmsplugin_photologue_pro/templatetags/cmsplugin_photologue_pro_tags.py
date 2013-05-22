97013b6b2a4426dfd0c5781a8e7b9f5b95235f78
Raphael Jasjukaitis <r.jasjukaitis@raphaa.de>
2013-05-22 17:12:51 +0200

    EXIF tags updated.

--- cmsplugin_photologue_pro/templatetags/cmsplugin_photologue_pro_tags.py
+++ cmsplugin_photologue_pro/templatetags/cmsplugin_photologue_pro_tags.py
@@ -5,6 +5,27 @@ from photologue import models
 
 register = template.Library()
 
+def get_exif_data(photo, attr):
+    value = str(photo.EXIF.get(attr, ''))
+    if attr == 'EXIF DateTimeOriginal':
+        dt = datetime.strptime(str(value), '%Y:%m:%d %H:%M:%S')
+        value = formats.date_format(dt, 'DATETIME_FORMAT', True)
+    elif attr == 'EXIF DateOriginal':
+        value = str(photo.EXIF.get('EXIF DateTimeOriginal', ''))
+        dt = datetime.strptime(str(value), '%Y:%m:%d %H:%M:%S')
+        value = formats.date_format(dt, 'DATE_FORMAT', True)
+    elif attr == 'EXIF ApertureValue':
+        f = value.split('/')
+        if len(f) == 1:
+            try:
+                aperture = float(f[0])
+            except ValueError:
+                return '-'
+        else:
+            aperture = float(f[0]) / float(f[1])
+        value = 'f/%.1f' % aperture
+    return value
+
 @register.inclusion_tag('cmsplugin_photologue_pro/polaroid.html')
 def polaroid_thumbnail(photo, photosize=None, counter=''):
     if not photosize:
@@ -18,18 +39,8 @@ def polaroid_thumbnail(photo, photosize=None, counter=''):
 
 @register.simple_tag
 def exif(photo, attr):
-    value = str(photo.EXIF.get(attr, ''))
-    if attr == 'EXIF DateTimeOriginal':
-        dt = datetime.strptime(str(value), '%Y:%m:%d %H:%M:%S')
-        value = formats.date_format(dt, 'DATETIME_FORMAT', True)
-    elif attr == 'EXIF ApertureValue':
-        f = value.split('/')
-        if len(f) == 1:
-            try:
-                aperture = float(f[0])
-            except ValueError:
-                return '-'
-        else:
-            aperture = float(f[0]) / float(f[1])
-        value = 'f/%.1f' % aperture
-    return value
+    return get_exif_data(photo, attr)
+
+@register.assignment_tag
+def get_exif(photo, attr):
+    return get_exif_data(photo, attr)
